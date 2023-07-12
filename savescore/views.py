b6dfaab47da04d1e4e4cf623cb299ff30c974c72
donghapark <donghaha12@naver.com>
2023-07-13 01:43:37 +0900

    테스트정보가져오기구현중

--- savescore/views.py
+++ savescore/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from models import Score
+from .models import Score
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ScoreSerializer
 
 
-
+class GetScore(APIView):
+    def get(self,request):
+        info=Score.objects.filter(testId=request.data["pk"]) 
+        info_serializer=ScoreSerializer(info)
+        return Response(info_serializer.data, status=200)
