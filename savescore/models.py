f647852b82f6b1dd2598db7a16e5d2ce1fc88ec7
donghapark <donghaha12@naver.com>
2023-07-12 23:54:02 +0900

    검사종류및디폴트설정

--- savescore/models.py
+++ savescore/models.py
@@ -3,15 +3,14 @@ from django.db import models
 
 class Score(models.Model):
     testId = models.AutoField(primary_key=True)
-    
-
-    score1 = models.DecimalField(max_digits=5, decimal_places=2,verbose_name='점수1')
-    score2 = models.DecimalField(max_digits=5, decimal_places=2,verbose_name='점수2')
-    score3 = models.DecimalField(max_digits=5, decimal_places=2,verbose_name='점수3')
-    score4 = models.DecimalField(max_digits=5, decimal_places=2,verbose_name='점수4')
-    score5 = models.DecimalField(max_digits=5, decimal_places=2,verbose_name='점수5')
-    score6 = models.DecimalField(max_digits=5, decimal_places=2,verbose_name='점수6')
-    score7 = models.DecimalField(max_digits=5, decimal_places=2,verbose_name='점수7')
-    score8 = models.DecimalField(max_digits=5, decimal_places=2,verbose_name='점수8')
-    score9 = models.DecimalField(max_digits=5, decimal_places=2,verbose_name='점수9')
-    score10 =models.DecimalField(max_digits=5, decimal_places=2,verbose_name='점수10')
+    test_type=models.IntegerField(null=True,blank=True,verbose_name='검사종류1~4',default=0)
+    score1 = models.DecimalField(null=True,blank=True,max_digits=5, decimal_places=2,verbose_name='점수1',default=0.0)
+    score2 = models.DecimalField(null=True,blank=True,max_digits=5, decimal_places=2,verbose_name='점수2',default=0.0)
+    score3 = models.DecimalField(null=True,blank=True,max_digits=5, decimal_places=2,verbose_name='점수3',default=0.0)
+    score4 = models.DecimalField(null=True,blank=True,max_digits=5, decimal_places=2,verbose_name='점수4',default=0.0)
+    score5 = models.DecimalField(null=True,blank=True,max_digits=5, decimal_places=2,verbose_name='점수5',default=0.0)
+    score6 = models.DecimalField(null=True,blank=True,max_digits=5, decimal_places=2,verbose_name='점수6',default=0.0)
+    score7 = models.DecimalField(null=True,blank=True,max_digits=5, decimal_places=2,verbose_name='점수7',default=0.0)
+    score8 = models.DecimalField(null=True,blank=True,max_digits=5, decimal_places=2,verbose_name='점수8',default=0.0)
+    score9 = models.DecimalField(null=True,blank=True,max_digits=5, decimal_places=2,verbose_name='점수9',default=0.0)
+    score10 =models.DecimalField(null=True,blank=True,max_digits=5, decimal_places=2,verbose_name='점수10',default=0.0)
